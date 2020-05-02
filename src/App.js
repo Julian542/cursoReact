f1d0d46097b258b5ddb93b9120e984cb389c4378
Julian542 <waltergc248@gmail.com>
2020-05-02 16:44:25 -0300

    1er commit, aprendiendo a usar las props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+//Utilizando una funcion que nos llega por parametros
+class Titulo extends Component{
+  render(){
+    const {
+      arrayOfNumbers,
+      isActivated, 
+      multiply,
+      title
+    } = this.props 
+      
+    const textoSegunBool = isActivated ? 'On' : 'Off'
+    const mappedNumbers = arrayOfNumbers.map(multiply) 
+    //esto es lo mismo que const mappedNumbers = this.props.arrayOfNumbers.map(this.props.multiply)
+    return(
+      <React.Fragment>
+        <p>{mappedNumbers.join(', ')}</p>
+        {title}
+      </React.Fragment>
+    );
+  }
+}
+//esto se hace para establecer valores por defecto a las props
+Titulo.defaultProps = {
+  title:<h1>Hola gente!</h1>   
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         <Titulo 
+            isActivated = {true}
+            arrayOfNumbers = {[2,4,6,8]}
+            multiply = { (number) => number *5 }
+         ></Titulo>
       </header>
     </div>
   );
