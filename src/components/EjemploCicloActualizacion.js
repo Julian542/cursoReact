6f26593e542dc1a8dfb03f9a362f14d9115919d9
Julian542 <waltergc248@gmail.com>
2020-05-05 18:36:49 -0300

    refactor del willreceiveprops

--- src/components/EjemploCicloActualizacion.js
+++ src/components/EjemploCicloActualizacion.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 const ANIMAL_IMAGES = {
+    panda:'https://goo.gl/oNbtoq',
     cat :'https://goo.gl/PoQQXb',
-    dolphin:'https://goo.gl/BbiKCd',
-    panda:'https://goo.gl/oNbtoq'
+    dolphin:'https://goo.gl/BbiKCd'
+    
 }
+
+const ANIMALS = Object.keys(ANIMAL_IMAGES);
 
 class AnimalImage extends Component{
     state = { src : ANIMAL_IMAGES[this.props.animal]}
@@ -13,7 +16,7 @@ class AnimalImage extends Component{
     componentWillReceiveProps (nextProps){ //este metodo nos ayuda a actualizar el componente justo cuando le van a llegar nuevas props
             this.setState({src: ANIMAL_IMAGES[nextProps.animal]})
     }
-    
+
     render(){
         return(
             <div>
@@ -29,23 +32,25 @@ class AnimalImage extends Component{
 }
 
 AnimalImage.propTypes ={
-    animal: PropTypes.oneOf(['cat','dolphin','panda'])
-}
-
-AnimalImage.defaultProps ={
-    animal:'panda'
+    animal: PropTypes.oneOf(ANIMALS)
 }
 
 class EjemploCicloActualizacion extends Component{
     state = {animal:'panda'}
 
+    _renderAnimalButton = (animal) => {
+        return(
+            <button
+                disabled={ animal === this.state.animal} 
+                key={animal} onClick={() => this.setState({animal})}> Ver {animal} </button>
+        );
+    }
+
     render(){
         return(
             <React.Fragment>
                 <h4>Ciclo de Actualizacion, ejemplo de: Component WillReceiveProps</h4>
-                <button onClick={() => this.setState({animal:'panda'})}> Ver Panda</button>
-                <button onClick={ () => this.setState({animal:'cat'})}>Ver Gatito</button>
-                <button onClick={ () => this.setState({animal:'dolphin'})}>Ver Delfin</button>
+                {ANIMALS.map( this._renderAnimalButton)}
                 <AnimalImage animal={this.state.animal}/>
             </React.Fragment>
         )
